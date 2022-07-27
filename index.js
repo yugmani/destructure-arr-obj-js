d5b260b2ff0cbde4f0fd62b81b75632e03153f00
yugmani <yugmani@yahoo.com>
2022-07-27 12:16:53 -0700

    introduction of array and object

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// **********************************
+// Creating an Array in JavaScript
+// **********************************
+
+//Array literal method
+const array1 = []; //empty array
+const array2 = ['HTML', 'CSS', 'JavaScript'];
+
+//Array constructor method
+const array3 = new Array(); //empty array
+
+const array4 = new Array(3); //[undefined, undefined, undefined]
+
+array4[0] = 'jQuery';
+(array4[1] = 'React'), (array4[2] = 'Vue');
+console.log(array4); // ["jQuery", "React", "Vue"]
+
+// **********************************
+// Creating an Object in JavaScript
+// **********************************
+
+//Object literal method
+const object1 = {};
+
+const object2 = {
+  name: 'John',
+  age: 33,
+};
