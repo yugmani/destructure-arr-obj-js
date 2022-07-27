9bf03a4ba7ee22a4c6473a43225fa8181f5ada24
yugmani <yugmani@yahoo.com>
2022-07-27 12:38:27 -0700

    destructuring arrays and swaping array values

--- index.js
+++ index.js
@@ -29,3 +29,36 @@ const object2 = {
   name: 'John',
   age: 33,
 };
+
+// **********************************
+// Destructuring Arrays in JavaScript
+// **********************************
+
+//Destructuring in Arrays
+// ********************************
+
+//How to assign variables with destructuring
+const colors = ['violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red'];
+
+const [first, second] = colors;
+console.log('First color: ', first); //violet
+console.log('Second color: ', second); //indigo
+
+//How to swap variables with destructuring
+const food = ['pizza', 'tortillas'];
+
+let [firstFood, secondFood] = food;
+console.log('first: ', firstFood); //first: pizza
+console.log('second: ', secondFood); //second: tortillas
+
+[firstFood, secondFood] = [secondFood, firstFood]; //swapping
+console.log('first: ', firstFood); //first: tortillas
+console.log('second: ', secondFood); //second: pizza
+
+console.log(food); //["pizza", "tortillas"]
+// See original array is NOT mutated.
+
+//Mutating by destructuring
+[food[0], food[1]] = [food[1], food[0]];
+
+console.log(food); //["tortillas", "pizza"];
